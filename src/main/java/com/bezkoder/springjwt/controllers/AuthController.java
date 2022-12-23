35e861f48bdbf9264e578b447f4e41a4fc7c4cf1
pm971857 <mullapudi.prabhudeva7@gmail.com>
2022-12-23 07:58:09 -0800

    updated code

--- src/main/java/com/bezkoder/springjwt/controllers/AuthController.java
+++ src/main/java/com/bezkoder/springjwt/controllers/AuthController.java
@@ -54,7 +54,7 @@ public class AuthController {
 
 	@PostMapping("/signin")
 	public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
-
+//public
 		Authentication authentication = authenticationManager.authenticate(
 				new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
 
